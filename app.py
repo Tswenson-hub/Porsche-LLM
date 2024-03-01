f325295672d4a2429c58394d079b9c330c006908
Tyler Swenson <57569000+Tswenson-hub@users.noreply.github.com>
2024-03-01 22:54:09 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from langchain.schema import(SystemMessage, HumanMessage, AIMessage)
+from main import complete_llm
+import os
+
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+st.write(os.environ)
+
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+if clear_button or "messages" not in st.session_state:
+    st.session_state.messages = [
+    SystemMessage(content="you are a helpful AI assistant. Reply your answer in markdown format.")
+]
+
+if user_input := st.chat_input("Input your question!"):
+    st.session_state.messages.append(HumanMessage(content=user_input))
+    with st.spinner("Bot is typing ..."):
+        answer = complete_llm(user_input)
+        st.session_state.messages.append(AIMessage(content=answer.response))
+else:
+    st.markdown("Can't think of anything? Try one of these prompts!")
+    st.info("Are Porsches showcased in movies and TV shows?")
+    st.info("Tell me a fact about Porsche Carreras that I didn't know.")
+    st.info("Do Porsches have a history in racing?")
+
+messages = st.session_state.get("messages", [])
+for message in messages:
+    if isinstance(message, AIMessage):
+        with st.chat_message("assistant"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.markdown(message.content)
